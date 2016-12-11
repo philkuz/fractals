d1afa6a91b7cd95db2690176778d46d84be75d80
philkuz <superpker@gmail.com>
2016-12-11 13:49:49 -0800

    Convert fractal and mandelbrot to numpy arrays. Not having success rendering the images

--- fractal.py
+++ fractal.py
@@ -1,5 +1,7 @@
-from PIL import Image
 import namer
+import matplotlib.pyplot as plt
+import numpy as np
+
 def andy_cs(i):
     ''' Color Scheme provided by Andrew Lewis '''
     r = i % 4 * 64
@@ -13,35 +15,35 @@ class Fractal:
     TODO: lookup what's a good abstract method way in python
     """
     def __init__(self, w, h):
-        self.w = w
-        self.h = h
-        self.pixel_mat = [[0 for _ in range(w)] for _ in range(h)]
+        self.pixel_mat = np.zeros((h, w))
         self.setup()
     def setup(self):
         ''' Used as a way to initialize instance variables without
         having to recreate a constructor '''
         pass
     def render(self):
-        self.pixel_mat = [[0 for _ in range(self.w)] for _ in range(self.h)]
+        self.pixel_mat = np.zeros(self.pixel_mat.shape)
+
+    def width(self):
+        return self.pixel_mat.shape[0]
+    def height(self):
+        return self.pixel_mat.shape[1]
+
     def in_image(self, x, y):
-        return x >= 0 and x < self.w and y >= 0 and y < self.h
+        return x >= 0 and x < self.width() and y >= 0 and y < self.height()
     def set_point(self, x, y, value):
-        self.pixel_mat[x][y] = value
+        self.pixel_mat[x,y] = value
     def get_point(self, x, y):
-        return self.pixel_mat[x][y]
+        return self.pixel_mat[x,y]
 
     def draw(self, image_name, colorFn=andy_cs):
         """ Draws the intensity values given by pixel_mat
         using the coloration described in colorFn, then saves
         the new image as image_name """
-        image = Image.new("RGB", (self.w, self.h))
-        mtx = image.load()
-        for x, row in enumerate(self.pixel_mat):
-            for y, entry in enumerate(row):
-                mtx[x, y] = colorFn(entry)
+        image = colorFn(self.pixel_mat) # TODO make color fn work for numpy
         file_name = namer.get_name(image_name)
-        image.save(file_name, "PNG")
+        plt.savefig(file_name)
+
     def print_matrix(self):
         """ Shorthand to nicely print out the rows of a matrix """
-        for row in self.pixel_mat:
-            print (row)
+        print(self.pixel_mat)
--- mandelbrot.py
+++ mandelbrot.py
@@ -1,64 +1,91 @@
 from fractal import Fractal
 import random
-import math
+import numpy as np
 
 class Mandelbrot(Fractal):
     """ A class that generates the mandelbrot fractal """
     def __init__(self, w=512, h=512, real_bounds=(-2.0, 1.0), imag_bounds=(-1.5, 1.5), iterations=512):
         Fractal.__init__(self, w, h)
-        self.rb = real_bounds
-        self.ib = imag_bounds
+        self.real_bounds = real_bounds
+        self.imag_bounds = imag_bounds
         self.threshold = abs(max(real_bounds + imag_bounds, key = lambda x: abs(x)))
         self.iterations = iterations
+        self.update = 5 # TODO make a parm
     def print_parameters(self):
-        print("\treal bounds:\t", self.rb)
-        print("\timag bounds:\t", self.ib)
+        print("\treal bounds:\t", self.real_bounds)
+        print("\timag bounds:\t", self.imag_bounds)
         print("\titerations:\t", self.iterations)
         print("\tthreshold:\t", self.threshold)
     def render(self):
-        """ Renders the mandelbrot set on pixel_mat
+        """ Renders the mandelbrot set into memory
         >>> m = Mandelbrot()
         >>> m.render()
         """
         Fractal.render(self)
-        lutx = [j * (self.rb[1] - self.rb[0]) / (self.w - 1) + self.rb[0] for j in range(self.w)]
-        for y in range(self.h):
-            cy = y * (self.ib[1] - self.ib[0]) / (self.h - 1)  + self.ib[0]
-            for x in range(self.w):
-                c = complex(lutx[x], cy)
-                iters = self.in_mandelbrot(0, c)
-                if iters > 0:
-                    self.set_point(x, y, iters)
+
+        lutx = np.arange(self.width())
+        lutx = lutx * (self.real_bounds[1] - self.real_bounds[0]) / (self.width() - 1) + self.real_bounds[0]
+        lutx = np.tile(lutx, (self.width(),1)).T
+
+        luty = np.arange(self.height())
+        luty = luty * (self.imag_bounds[1] - self.imag_bounds[0]) / (self.height() - 1)  + self.imag_bounds[0]
+        luty = np.tile(luty, (self.height(),1))
+
+        lut_mat = lutx + luty * 1j
+        iters = self.in_mandelbrot(np.zeros(lut_mat.shape), lut_mat)
+        self.set_pixel_mat(iters)
+
+    def set_pixel_mat(self, iters):
+        # TODO this may have to be changed if you want to transfrom the pixel.
+        # iters are just ierations. May have -1 if the complex point doesn't
+        # escape the image
+        self.pixel_mat = iters
+
     def in_mandelbrot(self, z, c):
-        """ Returns whether a point C lies in the mandelbrot set """
+        """ Returns whether points in the matrix C lie in the mandelbrot set """
         for i in range(self.iterations):
-            if abs(z) > self.threshold:
-                return i
-            z = z * z + c
-        return -1
+            abs_mask = np.abs(z) < self.threshold
+            z[abs_mask] = z[abs_mask] ** 2 + c[abs_mask]
+        z[abs_mask] = -1
+        return z
+
     def cardioid_test(self, z):
-        """ Returns whether a point is within the cardioid """
+        """
+        Returns whether a matrix of points are within the cardioid of the
+        mandelbrot
+        """
         x = z.real
         y = z.imag
-        q = math.pow(x- 0.25, 2) + math.pow(y, 2)
-        return q * (q + x - 0.25) < 0.25 * math.pow(y, 2)
+        q = (x - 0.25) ** 2 + y ** 2
+        return q * (q + x - 0.25) < 0.25 * y ** 2
 
     def sample_point(self):
         """ Samples the complex plane"""
+        # TODO make numpy
         z = complex(0, 0)
         while self.cardioid_test(z):
-            x = random.randint(0, self.w - 1)
-            y = random.randint(0, self.h - 1)
+            x = random.randint(0, self.width()- 1)
+            y = random.randint(0, self.height() - 1)
             z = self.image_to_complex(x, y)
         return z
-    def complex_to_image(self, z):
+    def complex_to_real(self, z):
+        # TODO make numpy
         """ Returns the complex number's position in the image """
-        x = int((z.real - self.rb[0]) / (self.rb[1] - self.rb[0]) * float(self.w - 1))
-        y = int((z.imag - self.ib[0]) / (self.ib[1] - self.ib[0]) * float(self.h - 1))
+        x = int((z.real - self.real_bounds[0]) / (self.real_bounds[1] - self.real_bounds[0]) * float(self.width()- 1))
+        y = int((z.imag - self.imag_bounds[0]) / (self.imag_bounds[1] - self.imag_bounds[0]) * float(self.height() - 1))
 
         return x, y
-    def image_to_complex(self, x, y):
+    def real_to_complex(self, x, y):
+        # TODO make numpy
         """ Returns the complex number of the coordinate in the image. """
-        real = x * (self.rb[1] - self.rb[0]) / float(self.w - 1) + self.rb[0]
-        imag = y * (self.ib[1] - self.ib[0]) / float(self.h - 1) + self.ib[0]
+        real = x * (self.real_bounds[1] - self.real_bounds[0]) / float(self.width()- 1) + self.real_bounds[0]
+        imag = y * (self.imag_bounds[1] - self.imag_bounds[0]) / float(self.height() - 1) + self.imag_bounds[0]
         return complex(real, imag)
+def atan_color(i, width=10):
+    ''' Returns a clamped color for any input based on the arctan function '''
+    return (255 * np.atan(float(i)/width)).astype(np.int)
+if __name__ == "__main__":
+    mandel = Mandelbrot(iterations=512 )
+    mandel.render()
+
+    mandel.draw("shite")
